24e399e633b334c0d6921a06210d90040c273663
Nathan Broadbent <git@ndbroadbent.com>
2020-05-20 03:05:56 +0800

    Update ga plugin for Heap (Gitbook 3.x)

--- book/plugin.js
+++ book/plugin.js
@@ -1,17 +1,13 @@
 require(["gitbook"], function(gitbook) {
-    // Load analytics.js
+    // Load Heap
     gitbook.events.bind("start", function(e, config) {
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-        var cfg = config.ga;
-        ga('create', cfg.token, cfg.configuration);
+        window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
+        var cfg = config.heap;
+        heap.load(cfg.token, cfg.configuration);
     });
 
-    // Notify pageview
-    gitbook.events.bind("page.change", function() {
-        ga('send', 'pageview', window.location.pathname+window.location.search);
-    });
+    // // Notify pageview
+    // gitbook.events.bind("page.change", function() {
+    //     ga('send', 'pageview', window.location.pathname+window.location.search);
+    // });
 });
